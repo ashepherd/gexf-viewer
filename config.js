fc7ea2e958f26ae983d07d9d957a5003cd90b019
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2024-01-06 12:02:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "memes_all.gexf",
+    graphFile : "bcodmo-dataset-people.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
